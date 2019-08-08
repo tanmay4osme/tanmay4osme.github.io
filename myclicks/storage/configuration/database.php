239f64748f398bd4a0889179c95a9df29ce7ad0c
Tanmay Moharana <38655754+tanmay-moharana@users.noreply.github.com>
2019-08-08 03:08:41 +0200

    NewWebsite

--- myclicks/storage/configuration/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-	return array(
-		'hostname' => 'localhost',
-		'database' => 'i3220506_koke1',
-		'username' => 'i3220506_koke1',
-		'password' => 'B.L8Wwoi6UQl&T6#Jm]31~.9',
-		'prefix' => 'koke_',
-		'socket' => ''
-	);
